b07501b4bf9f6529ee9e9f7cf81bf11ddcb95c9f
zeng2633085658 <37899921+zeng2633085658@users.noreply.github.com>
2018-05-18 19:26:45 +0800

    idea整合测试git

--- src/com/crm/test/DepVos.java
+++ src/com/crm/test/DepVos.java
@@ -24,7 +24,6 @@ public class DepVos {
 
     public static void main(String[] args) {
         System.out.println(AesUtils.encryptStr("123456",AesUtils.SECRETKEY));
-        System.out.println();
     }
 
 }
